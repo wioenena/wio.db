7d31ae0f6c2228e66cb22531908b47e2c0326ec6
wioenena-q <brs_kprl09@outlook.com>
2021-03-15 20:31:28 +0300

    🥳

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,7 +10,42 @@
 
 
 declare module "wio.db" {
-    export class Database<V> {
+    export class JsonDatabase<V> {
+        public static DBCollection: Array<Database<unknown>>;
+        private databaseName: string;
+        public constructor(databaseName?: string);
+        private handle(): boolean;
+        private save(): boolean;
+        public set(key: string, value: V): V;
+        public get(key: string): V;
+        public fetch(key: string): V;
+        public exists(key: string): boolean;
+        public has(key: string): boolean;
+        public all(limit?: number): Array<{ ID: string, data: V }>;
+        public fetchAll(limit?: number): Array<{ ID: string, data: V }>;
+        public toJSON(limit?: number): Object;
+        public delete(key: string): void;
+        public deleteAll(): void;
+        public type(key: string): string | number | bigint | boolean | symbol | Array | undefined | object | Function;
+        public pull(key: string, value: V, multiple?: boolean): V;
+        public valueArray(): V[];
+        public keyArray(): string[];
+        public math(key: string, operator: "+" | "-" | "*" | "/" | "%", value: V, goToNegative?: boolean): V;
+        public add(key: string, value: V): V;
+        public substr(key: string, value: V): V;
+        public push<T>(key: string, value: T): V;
+        public arrayHasValue<T>(key: string, value: T | T[]): boolean | object;
+        public includes(key: string): object;
+        public startsWith(key: string): object;
+        public findAndDelete(callbackfn: (key: string, value: V) => boolean): number;
+        public destroy(): void;
+        public get size(): number;
+        public get totalDBSize(): number;
+        public get fileName(): string;
+    }
+
+    export class YamlDatabase<V> {
+
         public static DBCollection: Array<Database<unknown>>;
         private databaseName: string;
         public constructor(databaseName?: string);
