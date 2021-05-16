077f9eed75e3c34dc0cccdb84256b568b0b017c4
wioenena-q <brs_kprl09@outlook.com>
2021-05-16 17:47:35 +0300

    Some bugs fixed.

--- test/main.test.js
+++ test/main.test.js
@@ -1,19 +1,5 @@
 const { JsonDatabase } = require("../index");
-new JsonDatabase(
-    {
-        databaseName: `${process.cwd()}/databases/example1/example1.json`
-    });
-new JsonDatabase({
-    databaseName: "./databases/example2/example2.json",
-});
-new JsonDatabase({
-    databaseName: "/databases/example3/example3.json",
-});
-new JsonDatabase(
-    {
-        databaseName: `${process.cwd()}./databases/example4/example4.json`
-    }
-);
+
 
 
 
@@ -25,7 +11,7 @@ describe("Json class all controls", () => {
         console.log(
             new JsonDatabase(
                 {
-                    databaseName: `${process.cwd()}/databases/example1/example1.json`
+                    databasePath: `${process.cwd()}/databases/example1/example1.json`
                 }
             ).path
         );
@@ -35,7 +21,7 @@ describe("Json class all controls", () => {
         console.log(
             new JsonDatabase(
                 {
-                    databaseName: "./databases/example2/example2.json"
+                    databasePath: "./databases/example2/example2.json"
                 }
             ).path
         );
@@ -45,7 +31,7 @@ describe("Json class all controls", () => {
         console.log(
             new JsonDatabase(
                 {
-                    databaseName: "/databases/example3/example3.json"
+                    databasePath: "/databases/example3/example3.json"
                 }
             ).path
         );
@@ -55,7 +41,7 @@ describe("Json class all controls", () => {
         console.log(
             new JsonDatabase(
                 {
-                    databaseName: `${process.cwd()}./databases/example4/example4.json`
+                    databasePath: `${process.cwd()}./databases/example4/example4.json`
                 }
             ).path
         );
@@ -65,7 +51,7 @@ describe("Json class all controls", () => {
         console.log(
             new JsonDatabase(
                 {
-                    maxData: "3" // -> Error
+                    maxDataSize: "3" // -> Error
                 }
             ).maxData
         );
@@ -75,7 +61,7 @@ describe("Json class all controls", () => {
         console.log(
             new JsonDatabase(
                 {
-                    maxData: -1 // -> Error
+                    maxDataSize: -1 // -> Error
                 }
             )
         );
@@ -83,8 +69,8 @@ describe("Json class all controls", () => {
 
     const db = new JsonDatabase(
         {
-            databaseName: "/databases/example3/example3.json",
-            maxData: 100
+            databasePath: "/databases/example3/example3.json",
+            maxDataSize: 100
         }
     );
     
